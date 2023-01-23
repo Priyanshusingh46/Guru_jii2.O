93a6aff05865c572127d5dcf01cb7cdd9d2e4e59
Priyanshu <priyanshusingh1877@gmail.com>
2023-01-23 18:00:03 +0530

    second

--- src/Components/Home/GuruFirstDiv.js
+++ src/Components/Home/GuruFirstDiv.js
@@ -2,6 +2,10 @@ import React from 'react'
 import demoImage from '../../Images/Home/DemoImage.png'
 import Home from '../../Css/Home.css'
 import chat from "../../Images/Home/chat.png"
+import Instagram from "../../Images/Home/instagram.png";
+import Facebook from "../../Images/Home/facebook.png";
+import Whatsaap from "../../Images/Home/whatsapp.png";
+import YouTube from "../../Images/Home/youtube.png";
 function GuruFirstDiv() {
   return (
     <div className='GuruFirstDivOuterBox'>
@@ -17,9 +21,20 @@ function GuruFirstDiv() {
     <div className='demoImageBox'>
         <img id="demoImage" src={demoImage} alt="error"/>
     </div>
+
+    <div className='SocialMedia'>
+
+      <div className='oneSocialMedia'>
+        <img src={Instagram} alt="error" />
+        <h4>Instagram</h4>
+      </div>
+
+    </div>
+
+
     </div>
     
-    <img id="chatImage" src={chat} alt="error" />
+    {/*<img id="chatImage" src={chat} alt="error" />*/}
     </div>
   )
 }
