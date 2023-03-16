4495d72955524b921904face1d45b6b88ac602e0
Priyanshu <priyanshusingh1877@gmail.com>
2023-03-16 12:06:20 +0530

    1st commit

--- src/Screens/About1.js
+++ src/Screens/About1.js
@@ -1,28 +1,26 @@
-import React from 'react'
-import AboutHeadlines from '../Components/About/AboutHeadlines'
-import Aboutphoto from '../Components/About/Aboutphoto'
-import Latestfromguru from '../Components/About/Latestfromguru'
-import MeetGurujii from '../Components/About/MeetGurujii'
-import Header from '../Components/Common/Header'
-import InfoGurujii from '../Components/Home/InfoGurujii'
-import Footer from "../Components/Home/Footer"
+import React from "react";
+import AboutHeadlines from "../Components/About/AboutHeadlines";
+import Aboutphoto from "../Components/About/Aboutphoto";
+import Latestfromguru from "../Components/About/Latestfromguru";
+import MeetGurujii from "../Components/About/MeetGurujii";
+import Header from "../Components/Common/Header";
+import InfoGurujii from "../Components/Home/InfoGurujii";
+import Footer from "../Components/Home/Footer";
 
 function About1() {
   return (
-    <div>
-        <Header />
-        <div style={{background: "#FFF6E4"}}>
+    <div style={{overflowX:"hidden"}}>
+      <Header />
+      <div style={{ background: "#FFF6E4" }}>
         <AboutHeadlines />
         <Aboutphoto />
         <InfoGurujii />
         <MeetGurujii />
         <Latestfromguru />
-        </div>
-        <Footer />
-       
-        
+      </div>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default About1
+export default About1;
