a0cf649de0b550a15a99582fe912950176600000
Priyanshu <priyanshusingh1877@gmail.com>
2023-03-17 15:14:59 +0530

    1st commit

--- src/Components/Home/Card.js
+++ src/Components/Home/Card.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import card1 from "../../Images/Card/card1.png"
 import "../../Css/Home/cards.css"
+import { Link } from 'react-router-dom'
+
 function Card({image,title}) {
+  
   return (
+    
     <div className='Cardouterdiv'>
-        <div className='cardimagediv'>     
-        <img id="cardimage"src={image} alt="error" />
+     
+        <div className='cardimagediv' >     
+        <Link to="/servicedescription"> <img id="cardimage"src={image} alt="error"  /></Link>
         </div>
         <h2 id="cardheading">{title}</h2>
         <p id="cardpara">
@@ -14,6 +19,7 @@ function Card({image,title}) {
 
 
     </div>
+    
   )
 }
 
