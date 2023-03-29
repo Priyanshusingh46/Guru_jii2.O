189eb6c4afd823271cbc5b4ba58b236e9c7f4723
Priyanshu <priyanshusingh1877@gmail.com>
2023-03-29 17:59:07 +0530

    imiitiall

--- src/Components/Product/Page1/firstdivsecondcard.js
+++ src/Components/Product/Page1/firstdivsecondcard.js
@@ -4,8 +4,10 @@ import css from "../../../Css/Product/First_page/secondcard.css"
 function firstdivsecondcard() {
   return (
     <div className='firstdivsecondcardouterdiv'>
-        <div>
+        <div className='cardItemsdiv'>
+          <div className='firstdivsecondcardimagedivcontainer'>
             <img src={image} id="firstdivsecondcardimage" alt="error"/>
+            </div>
             <div className='firstdivsecondcardimagetextandcost'>
             <p id="firstdivsecondcardimagetextandcosttext">Rudraksh</p>
             <p id="firstdivsecondcardimagetextandcosttext">$ 27,000</p>
