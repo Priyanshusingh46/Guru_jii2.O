518b47c79c28e892c8722e61e09d1c3d5dcc8969
Priyanshu46 <priyanshusingh1877@gmail.com>
2023-04-07 19:50:24 +0530

    resolve error

--- src/Components/Home/TestimonalCard.js
+++ src/Components/Home/TestimonalCard.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import image from "../../Images/Card/me.jpg"
 import css from "../../Css/Home/Testimonalcard.css"
-function TestimonalCard(props) {
-  console.log(props.data.description);
+function TestimonalCard({name ,desc ,desg ,imge}) {
+  //console.log(props.data.description);
   return (
     <div className='Testimonalcardouterdiv'>
-        <p id="texts">{props.data.description}</p>
+        <p id="texts">{desc}</p>
         <div className='Personinfo'>
-            <p id="texts">{props.data.name}{props.data.designation}</p>
-            <img id="image"src={props.data.image} alt="error"/>
+            <p id="texts">{name}{desg}</p>
+            <img id="image"src= {imge} alt="error"/>
         </div>
     </div>
   )
--- src/Components/Home/Testimonial.js
+++ src/Components/Home/Testimonial.js
@@ -10,6 +10,19 @@ function Testimonial() {
   const[data1,setData1] = useState(null);
   const[data2,setData2] = useState(null);
 
+  const[desc,setDesc] =useState(null);
+  const[desc1,setDesc1] =useState(null);
+  const[desc2,setDesc2] =useState(null);
+
+  const[desg,setDesg] =useState(null);
+  const[desg1,setDesg1] =useState(null);
+  const[desg2,setDesg2] =useState(null);
+
+  const[img,setImg] =useState(null);
+  const[img1,setImg1] =useState(null);
+  const[img2,setImg2] =useState(null);
+
+
   useEffect(() => {
     getItems();
   }, [])
@@ -17,10 +30,23 @@ function Testimonial() {
   const getItems=async()=>{
     try{
     let result = await axios.get("https://subhashishgurujii.onrender.com/home/testimonal");
-    setData(result.data[0]);
-    setData1(result.data[1]);
-    setData2(result.data[2]);
-    //console.log(result.data[0]);
+    setData(result.data[0].name);
+    setData1(result.data[1].name);
+    setData2(result.data[2].name);
+
+    setDesc(result.data[0].description)
+    setDesc1(result.data[1].description)
+    setDesc2(result.data[2].description)
+
+    setDesg(result.data[0].designation)
+    setDesg1(result.data[1].designation)
+    setDesg2(result.data[2].designation)
+
+    setImg(result.data[0].image)
+    setImg1(result.data[1].image)
+    setImg2(result.data[2].image)
+
+    console.log(result.data[0]);
     }
     catch(e){
       console.log(e);
@@ -40,9 +66,9 @@ function Testimonial() {
         </div>
 
         <div className='Testimonalinfodiv'>
-            <TestimonalCard data={data}/>
-            <TestimonalCard data={data1}/>
-            <TestimonalCard data={data2}/>
+            <TestimonalCard name ={data} desc ={desc} desg ={desg} imge ={img}/>
+            <TestimonalCard name ={data1} desc ={desc1} desg ={desg1} imge ={img1} />
+            <TestimonalCard name ={data2} desc ={desc2} desg ={desg2} imge ={img2} />
         </div>
 
         <div className='Clientdataouterdiv'>
